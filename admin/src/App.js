daf1e4fb22a45106755505659bdbbe64f53479d9
Baldrani <mael.mayon@free.fr>
2019-02-15 13:51:14 +0100

    4 save purpose

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,4 +1,13 @@
 import React from 'react';
 import { HydraAdmin } from '@api-platform/admin';
+import authProvider from './authProvider';
+import Admin from './Admin'
 
+const App = () => (
+  <Admin authProvider={authProvider}>
+  ...
+  </Admin>
+);
+
+//export default () => App();
 export default () => <HydraAdmin entrypoint={process.env.REACT_APP_API_ENTRYPOINT}/>;
